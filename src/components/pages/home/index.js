05e92486c44be39eadf5499af7366426a04bc561
Stranniy1424 <stranniy1424@gmail.com>
2024-03-31 08:12:50 +0200

    #1 commit main

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,23 +1,9 @@
 import React from 'react';
 
 import Header from '../../header';
-import Slider from "./slider"
 import '../../../css/pages/home/homeMain.scss'
 
-function Home() {
-    // const [cars, setCars] = React.useState([]);
-
-    // React.useEffect(()=>{
-    //   fetch('https://66084611a2a5dd477b14432d.mockapi.io/api/iktzap/CARS')
-    //   .then((res) => {
-    //     return res.json();
-    //   })
-    //   .then((json) => {
-    //     setCars(cars)
-    //   });
-    // }, []);
-
-
+function home() {
   return (
     <>
     <Header/>
@@ -25,7 +11,20 @@ function Home() {
         <div className="container">
             <div className="item">
                 <div className="slider">
-                    <Slider/>
+                    <div className="text">
+                        <h1 className="title">
+                            SANY SY500H <b>или аналог</b>
+                        </h1>
+                        <ul>
+                            <li><img src="img/done.png" alt="" />Эксплуатационная масса: 50т</li>
+                            <li><img src="img/done.png" alt="" />Длинна стрелы : 7м</li>
+                            <li><img src="img/done.png" alt="" />Длинна стрелы : 3м</li>
+                            <li><img src="img/done.png" alt="" />Длинна стрелы : 7м</li>
+                            <li><img src="img/done.png" alt="" />Объем ковша : 2,5 м3</li>
+                        </ul>
+                        <button>Подробнее...</button>
+                    </div>
+                    <img className='ekskovator' src="img/Cars/Эксковатор.jpg" alt="" />
                 <div className="button">
                     <img className='left' src="img/right.svg" alt="" />
                     <img className='right' src="img/right.svg" alt="" />
@@ -39,4 +38,4 @@ function Home() {
 }
 
 
-export default Home;
+export default home;
