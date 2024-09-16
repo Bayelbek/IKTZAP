6a9dc5307d7c7ff44ff2551364cdbe81fd4bcab7
Bekbolot Tazhibaev <tabekg@gmail.com>
2024-09-16 08:54:24 +0000

    merge conflicts

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -3,12 +3,9 @@ import { Link } from "react-router-dom";
 import Header from "../../header";
 import Footer from '../../footer'
 import "../../../css/pages/home/homeMain.scss";
-<<<<<<< HEAD
 import NewsComponent from "./NewsComponent";
 import NewsFeed from "./NewsFeed";
-=======
 import Slider from "./Slider/Slider.tsx";
->>>>>>> d3d66e9164e1d37c483480d182263f188ec23275
 
 function home() {
   return (
